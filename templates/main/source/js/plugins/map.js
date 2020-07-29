e2533feb67a72502401c7e765afab951c9b4693b
RadioMan116 <noris75564@gmail.com>
2020-07-30 00:52:57 +0300

    #контент Доработка верстки МОБ-версии сайта smeg.ret-team.ru

--- templates/main/source/js/plugins/map.js
+++ templates/main/source/js/plugins/map.js
@@ -1,86 +1,90 @@
 $(document).ready(function () {
+	let checkMap = document.querySelector(".maps")
 
-	var myPlacemark;
-	var myMap;
+	if (checkMap) {
+		var myPlacemark;
+		var myMap;
 
-	function createMaps(element, coords, center, zoom) {
+		function createMaps(element, coords, center, zoom) {
 
-		if (!element) {
-			return false;
-		}
+			if (!element) {
+				return false;
+			}
 
-		ymaps.ready(function () {
+			ymaps.ready(function () {
 
-			myPlacemark;
-			myMap = new ymaps.Map(element, {
-				center: [parseFloat(center[0]), parseFloat(center[1])],
-				zoom: zoom
-			}, {
-				searchControlProvider: 'yandex#search'
-			});
-
-			myMap.controls.remove('searchControl');
-			myMap.behaviors.disable('scrollZoom');
-
-			for (var i = 0, l = coords.length; i < l; i += 1) {
-
-				myPlacemark = new ymaps.Placemark([parseFloat(coords[i].x), parseFloat(coords[i].y)], {
-					iconCaption: coords[i].c,
-					balloonContent: coords[i].t,
+				myPlacemark;
+				myMap = new ymaps.Map(element, {
+					center: [parseFloat(center[0]), parseFloat(center[1])],
+					zoom: zoom
 				}, {
-					preset: 'islands#redIcon',
+					searchControlProvider: 'yandex#search'
 				});
 
-				// myPlacemark.options
-				// 	// Во избежание двойной реакции геообъекта
-				// 	// на изменение опций сначала вызываем freeze, а после задания
-				// 	// всех значений вызываем unfreeze.
-				// 	.freeze()
-				// 	.unfreeze();
+				myMap.controls.remove('searchControl');
+				myMap.behaviors.disable('scrollZoom');
 
-				myMap.geoObjects.add(myPlacemark);
+				for (var i = 0, l = coords.length; i < l; i += 1) {
 
-			}
-		});
+					myPlacemark = new ymaps.Placemark([parseFloat(coords[i].x), parseFloat(coords[i].y)], {
+						iconCaption: coords[i].c,
+						balloonContent: coords[i].t,
+					}, {
+						preset: 'islands#redIcon',
+					});
 
-	}
+					// myPlacemark.options
+					// 	// Во избежание двойной реакции геообъекта
+					// 	// на изменение опций сначала вызываем freeze, а после задания
+					// 	// всех значений вызываем unfreeze.
+					// 	.freeze()
+					// 	.unfreeze();
 
-	// contact map
-	(function () {
-		var items = document.querySelectorAll('.js-warranty__map-point');
-		var map = document.getElementById('js-warranty__map');
+					myMap.geoObjects.add(myPlacemark);
 
-		var center = (map.getAttribute('data-center')).split(',');
-		if (!map) {
-			return false;
+				}
+			});
+
 		}
 
-		// var tmp = (map.getAttribute('data-coords')).split(',');
-		var coords = []
-		// coords[0] = {
-		// 	x: tmp[0],
-		// 	y: tmp[1]
-		// }
-		$(items).each(function (index) {
+		// contact map
+		(function () {
+			var items = document.querySelectorAll('.js-warranty__map-point');
+			var map = document.getElementById('js-warranty__map');
 
-			var tmp = (this.getAttribute('data-coords')).split('||')
-			// var tmp = ();
-			// var tmp = (this.getAttribute('data-text'));
-
-			coords[index] = {
-				x: tmp[0],
-				y: tmp[1],
-				c: tmp[2],
-				t: tmp[3]
+			var center = (map.getAttribute('data-center')).split(',');
+			if (!map) {
+				return false;
 			}
 
-		});
-		console.log(coords)
+			// var tmp = (map.getAttribute('data-coords')).split(',');
+			var coords = []
+			// coords[0] = {
+			// 	x: tmp[0],
+			// 	y: tmp[1]
+			// }
+			$(items).each(function (index) {
 
-		createMaps(map, coords, center, 9);
+				var tmp = (this.getAttribute('data-coords')).split('||')
+				// var tmp = ();
+				// var tmp = (this.getAttribute('data-text'));
 
-		// createMaps(map, coords, tmp, 14);
+				coords[index] = {
+					x: tmp[0],
+					y: tmp[1],
+					c: tmp[2],
+					t: tmp[3]
+				}
 
-	})();
+			});
+			console.log(coords)
+
+			createMaps(map, coords, center, 9);
+
+			// createMaps(map, coords, tmp, 14);
+
+		})();
+	}
+
 
 });
